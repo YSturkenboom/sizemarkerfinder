90bd2390788cb75cfbf9225725deca612500b379
Ysturkenboom <y.a.sturkenboom@gmail.com>
2019-08-24 00:34:32 +0200

    timing test run

--- evaluateModel.py
+++ evaluateModel.py
@@ -54,7 +54,7 @@ def makePredictions(prediction_data_path, plt, n, title):
     pred_data = np.divide(pred_data, 25000)
 
     pred_data = np.expand_dims(pred_data, axis=0)
-    pred_date = pred_data[:,:,:2]
+    pred_data = pred_data[:,:,:2]
     print(pred_data.shape, pred_data)
     
     predictions = model.predict(pred_data)
